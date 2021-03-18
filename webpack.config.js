9d076aec0657fd195b3f8ff5e0284fbcaf99b502
HeflerDev <heflerdev@gmail.com>
2021-03-18 02:32:16 -0300

    Release/1.3

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './index.js',
-  mode: 'development',
+  mode: 'production',
   output: { 
     filename: 'app.js',
     path: path.resolve( __dirname, 'dist'),
